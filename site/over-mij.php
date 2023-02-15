24d82763465bbfe37bb4e24923d49b11ac36eace
Keiji <mikeykouw@hotmail.com>
2023-02-15 13:42:51 +0100

    finish maybe 22

--- site/over-mij.php
+++ site/over-mij.php
@@ -2,9 +2,7 @@
     <nav>
         <ul>
             <li>
-                <a href="show.php?waarde1=Voornaam: Mikey
-                  waarde2=Achternaam: Smit
-                 waarde3=Leeftijd: 18">Mijn Gegevens</a>
+                <a href="show.php?waarde1=Voornaam: Mikey <div> Achternaam: Smit <div> Leeftijd: 18">Mijn Gegevens</a>
             </li>
         </ul>
 
