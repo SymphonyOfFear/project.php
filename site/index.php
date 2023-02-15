47ba3ba35387afdbcaa736961601b8dcd5935276
Keiji <mikeykouw@hotmail.com>
2023-02-15 13:03:07 +0100

    "opdracht 21"

--- site/index.php
+++ site/index.php
@@ -1,3 +1,16 @@
 <?php
 
-phpinfo();
+
+$a = 1250;
+$b = 222;
+
+if($a < $b)
+{
+    echo '$a is smaller then $b';
+}
+ else{
+    echo'$b is smaller then $a';
+}
+
+
+?>
---
+++ site/select.php
@@ -0,0 +1,18 @@
+<body>
+    <nav>
+        <ul>
+            <li>
+                <a href="show.php">Show zonder waarde</a>
+            </li>
+            <li>
+                <a href="show.php?waarde1=Honderd">Show met 1 waarde</a>
+            </li>
+            <li>
+                <a href="show.php?waarde1=TweeHonderd&waarde2=Drie">Show met 2 waardes</a>
+            </li>
+            <li>
+                <a href="show.php?waarde1=Snoop%20Dog&waarde2=Dr%20Dre">Snoop dog en Dr Dre</a>
+            </li>
+        </ul>
+    </nav>
+</body>
