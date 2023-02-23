78af509f52aa40fc8841eca47ea10be47351e91b
Keiji <mikeykouw@hotmail.com>
2023-02-23 13:30:19 +0100

    data importeren update 1

---
+++ site/functions-testing.php
@@ -0,0 +1,26 @@
+<?php
+$naam = "";
+$leeftijd = 0;
+$isGetrouwd = false;
+$kinderen= array();
+
+if($naam == empty(true))
+{
+    var_dump($naam);
+    echo $naam;
+}
+if($leeftijd == empty(0))
+{
+    var_dump($leeftijd);
+    echo $leeftijd;
+}
+if($isGetrouwd == empty(false))
+{
+    var_dump($naam);
+    echo $isGetrouwd;
+}
+if($kinderen == empty(true))
+{
+    var_dump($kinderen);
+    echo $kinderen;
+}
